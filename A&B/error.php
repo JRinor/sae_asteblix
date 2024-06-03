0e0d78d246cebcb78cdfa71baae2b066a3c5b149
NeggutsMc <negguts_mc@outlook.com>
2024-06-03 17:44:57 +0200

    main

---
+++ A&B/error.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Erreur</title>
+    <meta charset="utf-8">
+</head>
+<body>
+<h1>Une erreur est survenue</h1>
+<p>Les identifiants que vous avez entrés sont incorrects. Veuillez réessayer.</p>
+<a href="index.php">Retour à la page d'accueil</a>
+</body>
+</html>
